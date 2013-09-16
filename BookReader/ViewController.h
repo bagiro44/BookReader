13eff41a631fcd82e355c5b6aa154c1972a53c08
bagiro44 <dmitriyremezov@MacBook-Air-Dmitriy.local>
2013-09-16 20:00:16 +0400

    Initial Commit

---
+++ BookReader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BookReader
+//
+//  Created by Dmitriy Remezov on 16.09.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BookReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BookReader
+//
+//  Created by Dmitriy Remezov on 16.09.13.
+//  Copyright (c) 2013 Dmitriy Remezov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
